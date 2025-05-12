3bd91e8b1ef1a1383d91d2f722a84bb6b91de664
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2025-05-12 17:46:56 +0300

    Control 500 errors

--- src/app/api/sessions/[sessionId]/route.ts
+++ src/app/api/sessions/[sessionId]/route.ts
@@ -13,50 +13,59 @@ export async function POST(
   req: NextRequest,
   { params }: { params: Promise<{ sessionId: string }> }
 ) {
-  const { sessionId } = await params;
-  const startPage = parseInt(
-    req.nextUrl.searchParams.get("startPage") || "1",
-    10
-  );
-  const endPage = parseInt(req.nextUrl.searchParams.get("endPage") || "1", 10);
-  const {details: sourceDetails}: SessionProgress<PutBlobResult> = JSON.parse(await redis.get(`${sessionId}/progress`) as string);
+  try {
 
-  await redis.set(
-    `${sessionId}/progress`,
-    JSON.stringify({ stage: "IDLE", cursor: 0 })
-  );
-
-  const document = await getDocument(sourceDetails.url).promise;
-
-  const canvasFactory = document.canvasFactory;
-  const [images, scan] = useScanner(canvasFactory, 1);
-  const scannedPages = [];
-  await parallelReading(document.numPages, async (pageNum: number) => {
-    const page = await document.getPage(pageNum);
-    await scan(pageNum, page);
-    scannedPages.push(pageNum);
-    await redis.set(`${sessionId}/progress`, JSON.stringify({ stage: "SCANNING", cursor: pageNum, progress: Math.floor((scannedPages.length / document.numPages) * 100), details: "" }));
-  });
-
-  const [sheet, extract] = await useSheeter({ readingMemoryLimit: 15 });
-  for (let i = startPage; i <= endPage; i++) {
-    const image = images(i) as string;
-    const lines = await extract(i, image);
-    await redis.set(`${sessionId}/progress`, JSON.stringify({ stage: "EXTRACTING", cursor: i, progress: Math.floor((i / document.numPages) * 100), details: JSON.stringify(lines) }));
+    const { sessionId } = await params;
+    const startPage = parseInt(
+      req.nextUrl.searchParams.get("startPage") || "1",
+      10
+    );
+    const endPage = parseInt(req.nextUrl.searchParams.get("endPage") || "1", 10);
+    const {details: sourceDetails}: SessionProgress<PutBlobResult> = JSON.parse(await redis.get(`${sessionId}/progress`) as string);
+  
+    await redis.set(
+      `${sessionId}/progress`,
+      JSON.stringify({ stage: "IDLE", cursor: 0 })
+    );
+  
+    const document = await getDocument(sourceDetails.url).promise;
+  
+    const canvasFactory = document.canvasFactory;
+    const [images, scan] = useScanner(canvasFactory, 1);
+    const scannedPages = [];
+    await parallelReading(document.numPages, async (pageNum: number) => {
+      const page = await document.getPage(pageNum);
+      await scan(pageNum, page);
+      scannedPages.push(pageNum);
+      await redis.set(`${sessionId}/progress`, JSON.stringify({ stage: "SCANNING", cursor: pageNum, progress: Math.floor((scannedPages.length / document.numPages) * 100), details: "" }));
+    });
+  
+    const [sheet, extract] = await useSheeter({ readingMemoryLimit: 15 });
+    for (let i = startPage; i <= endPage; i++) {
+      const image = images(i) as string;
+      const lines = await extract(i, image);
+      await redis.set(`${sessionId}/progress`, JSON.stringify({ stage: "EXTRACTING", cursor: i, progress: Math.floor((i / document.numPages) * 100), details: JSON.stringify(lines) }));
+    }
+  
+    const sheetFile: SheetFile = { pdfFilename: sourceDetails.pathname, sheet };
+    await redis.set(
+      `${sessionId}/sheet`,
+      JSON.stringify(sheetFile),
+      "EX",
+      60 * 60 * 5
+    );
+    const sheetUrl = new URL(`/api/sessions/${sessionId}`, req.url).toString();
+  
+    await del(sourceDetails.url);
+  
+    return NextResponse.json({ sheetUrl }, { status: 200 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: "An error occurred", details: error.message }, { status: 500 });
+    } else {
+      return NextResponse.json({ error: "An error occurred", details: error }, { status: 500 });
+    }
   }
-
-  const sheetFile: SheetFile = { pdfFilename: sourceDetails.pathname, sheet };
-  await redis.set(
-    `${sessionId}/sheet`,
-    JSON.stringify(sheetFile),
-    "EX",
-    60 * 60 * 5
-  );
-  const sheetUrl = new URL(`/api/sessions/${sessionId}`, req.url).toString();
-
-  await del(sourceDetails.url);
-
-  return NextResponse.json({ sheetUrl }, { status: 200 });
 }
 
 export async function GET(
