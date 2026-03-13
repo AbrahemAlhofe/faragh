6e5160dbd8ece3c0ac5a4d31d6d6bc61d00e6112
Ibrahim Elhofy <alhofeabrahem88@gmail.com>
2026-03-13 17:20:05 +0200

    debug: migrate build time logging to new instrumentation module

---
+++ instrumentation.ts
@@ -0,0 +1,5 @@
+export function register () {
+    console.log(`[INFO] Build Time : ${process.env.buildTime}`);
+    console.log(`[INFO] Version : ${process.env.version}`);
+    console.log(`[INFO] Node Environment : ${process.env.NODE_ENV}`);
+}
