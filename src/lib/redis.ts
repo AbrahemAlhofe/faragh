983d1c9b128a4121d368616970cf0c9ee64c9b02
Ibrahim Elhofy <alhofeabrahem88@gmail.com>
2026-03-13 02:07:41 +0200

    refact: enhance Redis connection handling

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,8 +3,12 @@ import Redis from "ioredis";
 let redisInstance: Redis | null = null;
 
 export function getRedis(): Redis {
+  // For production, we have a username and password, but for development we might not, so we conditionally include them in the Redis constructor
   if (!redisInstance) {
-    redisInstance = new Redis(6379, 'redis');
+    redisInstance = new Redis(parseInt(process.env.REDIS_URL || "6379"), process.env.REDIS_HOST || "localhost", {
+        username: process.env.REDIS_USERNAME || undefined,  
+        password: process.env.REDIS_PASSWORD || undefined
+    });
   }
   return redisInstance;
 }
